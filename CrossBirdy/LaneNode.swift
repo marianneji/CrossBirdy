3ac62a0a63aef98ca70e1da39cec2b8c271f26f1
marianneji <marianneji@gmail.com>
2020-03-09 10:54:24 +0100

    created LaneType with his class and a func to create Lanes

---
+++ CrossBirdy/LaneNode.swift
@@ -0,0 +1,52 @@
+//
+//  LaneNode.swift
+//  CrossBirdy
+//
+//  Created by Graphic Influence on 06/03/2020.
+//  Copyright © 2020 marianne massé. All rights reserved.
+//
+
+import SceneKit
+
+enum LaneType {
+    case grass, road
+}
+
+class LaneNode: SCNNode {
+
+    var type: LaneType
+
+    init(type: LaneType, width: CGFloat) {
+        self.type = type
+        super.init()
+
+        switch type {
+        case .grass:
+            guard let texture = UIImage(named: "art.scnassets/grass.png") else {
+                break
+            }
+            createLane(width: width, Height: 0.4, image: texture)
+
+        case .road:
+            guard let texture = UIImage(named: "art.scnassets/asphalt.png") else {
+                break
+            }
+            createLane(width: width, Height: 0.05, image: texture)
+        }
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func createLane(width: CGFloat, Height: CGFloat, image: UIImage) {
+        let Lanegeometry = SCNBox(width: width, height: Height, length: 1, chamferRadius: 0)
+        Lanegeometry.firstMaterial?.diffuse.contents = image
+        Lanegeometry.firstMaterial?.diffuse.wrapT = .repeat
+        Lanegeometry.firstMaterial?.diffuse.wrapS = .repeat
+        Lanegeometry.firstMaterial?.diffuse.contentsTransform = SCNMatrix4MakeScale(Float(width), 1, 1)
+        let laneNode = SCNNode(geometry: Lanegeometry)
+        addChildNode(laneNode)
+    }
+
+}
